9b910e3c5c814183528209f9400d10689c77ac24
Zhaomin <zhaomin@comp.nus.edu.sg>
2022-06-27 18:39:42 +0800

    Update python files.

--- python-utils/plot.py
+++ python-utils/plot.py
@@ -187,11 +187,11 @@ if __name__ == '__main__':
     parser.add_argument('--output-dir', '-o', type=str, default='../fig/')
     args = parser.parse_args()
     dataset = 'codrna'
-    n_trees = 10
+    n_trees = 1
     ratio = '1e-02'
     # visualize(f"../cache/{dataset}_tree{n_trees}_original_{ratio}_deleted.json", 'deltaboostv2', f'fig/tree_structure/{dataset}_tree{n_trees}/')
     visualize(f"../cache/{dataset}_tree{n_trees}_original_{ratio}_deltaboost.json", 'deltaboostv2', f'fig/tree_structure/{dataset}_tree{n_trees}/')
-    # visualize(f"../cache/{dataset}_tree{n_trees}_retrain_{ratio}_deltaboost.json", 'deltaboostv2', f'fig/tree_structure/{dataset}_tree{n_trees}/')
+    visualize(f"../cache/{dataset}_tree{n_trees}_retrain_{ratio}_deltaboost.json", 'deltaboostv2', f'fig/tree_structure/{dataset}_tree{n_trees}/')
 
     # visualize(args.path, model_type=args.model_type, output_dir=args.output_dir)
 
--- python-utils/test.py
+++ python-utils/test.py
@@ -4,7 +4,7 @@ import ujson as json
 import numpy as np
 
 dataset = "codrna"
-retrain = GBDT.load_from_json(json.load(open(f"../cache/{dataset}_tree10_retrain_1e-02_deltaboost.json")), 'deltaboost')
-delete = GBDT.load_from_json(json.load(open(f"../cache/{dataset}_tree10_original_1e-02_deleted.json")), 'deltaboost')
-for i in range(10):
+retrain = GBDT.load_from_json(json.load(open(f"../cache/{dataset}_tree1_retrain_1e-02_deltaboost.json")), 'deltaboost')
+delete = GBDT.load_from_json(json.load(open(f"../cache/{dataset}_tree1_original_1e-02_deleted.json")), 'deltaboost')
+for i in range(1):
     print(f"Tree{i}: {retrain.trees[i] == delete.trees[i]}")
