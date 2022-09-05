2f3810d13dc62a9086dc15434294fa8009ce5ac6
Zhaomin <zhaomin@comp.nus.edu.sg>
2022-09-05 18:35:34 +0800

    Tuned hyperparameters. The robustness on all five dataset are good. Among them, only cadata with removal 1e-02 does not reach 0 score. DeltaBoost is perfectly robust on other datasets in the first tree.

--- test_remove_seq.sh
+++ test_remove_seq.sh
@@ -16,8 +16,8 @@ for dataset in codrna cadata covtype gisette msd; do
       taskset -c $cpus ./main conf/"$dataset"_1e-02.conf data=./data/"$dataset".train.remain_1e-02 remove_ratio="$ratio" n_trees=$n_trees \
           save_model_name="$dataset"_tree"$n_trees"_retrain_1e-02 remain_data=./data/"$dataset".train.remain_1e-02 delete_data=./data/"$dataset".train.delete_1e-02 > \
           $outdir/"$dataset"_deltaboost_"$ratio"_retrain.out
-#      taskset -c $cpus ./main conf/"$dataset"_1e-02.conf save_model_name="$dataset"_tree"$n_trees"_gbdt data=./data/"$dataset".train enable_delta=false n_trees=$n_trees > \
-#        $outdir/"$dataset"_gbdt.out
+      taskset -c $cpus ./main conf/"$dataset"_1e-02.conf save_model_name="$dataset"_tree"$n_trees"_gbdt data=./data/"$dataset".train enable_delta=false n_trees=$n_trees > \
+        $outdir/"$dataset"_gbdt.out
     else
       if [ $ratio = "0.001" ]; then
         taskset -c $cpus ./main conf/"$dataset"_1e-03.conf data=./data/"$dataset".train remove_ratio="$ratio" n_trees=$n_trees \
