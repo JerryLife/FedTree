49774d4d035793e9bbf56d2858ea818443583cad
Zhaomin <zhaomin@comp.nus.edu.sg>
2022-10-15 14:56:38 +0800

    Tuned hyperparameters. Add python environment.

---
+++ test_remove_gbdt_efficiency.sh
@@ -0,0 +1,25 @@
+n_trees=$1  # 1, 10, 30, 100
+dataset=$2  # cadata, codrna, gisette, covtype, msd, higgs
+cpus="0-63"
+
+: "${1?"Number of trees unset."}"
+: "${1?"Dataset unset."}"
+: "${1?"Ratio unset."}"
+: "${1?"Number of rounds unset."}"
+
+subdir=tree"$n_trees"
+outdir="out/remove_test/$subdir/"
+mkdir -p $outdir
+
+
+# run one time because GBDT is determinstic
+taskset -c $cpus ./main conf/"$subdir"/"$dataset"_1e-02.conf save_model_name="$dataset"_tree"$n_trees"_gbdt_1e-02 \
+          data=./data/"$dataset".train.remain_1e-02 enable_delta=false n_trees=$n_trees > \
+          $outdir/"$dataset"_gbdt_1e-02.out
+
+taskset -c $cpus ./main conf/"$subdir"/"$dataset"_1e-03.conf save_model_name="$dataset"_tree"$n_trees"_gbdt_1e-03 \
+          data=./data/"$dataset".train.remain_1e-03 enable_delta=false n_trees=$n_trees > \
+          $outdir/"$dataset"_gbdt_1e-03.out
+
+
+
